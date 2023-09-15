f411cd9a5d7bb4b27dd5456ccc646d15ed4c8131
Soumya Prakash Sahu <52891015+SOUMYA-CODING@users.noreply.github.com>
2023-09-15 11:51:35 +0530

    changes in featured section

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -13,12 +13,11 @@ const Featured = () => {
                     <Image src="/p1.jpeg" alt="" fill className={styles.image} />
                 </div>
                 <div className={styles.textContainer}>
-                    <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet alim consectetur adipisicing elit.</h1>
+                    <h1 className={styles.postTitle}>{`The Pink City's Royal Charm`}</h1>
                     <p className={styles.postDesc}>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Cupiditate, quam nisi magni ea laborum inventore voluptatum
-                        laudantium repellat ducimus unde aspernatur fuga. Quo, accusantium
-                        quisquam! Harum unde sit culpa debitis.
+                        {`
+                        Nestled in the heart of Rajasthan, Jaipur, often referred to as the "Pink City," is a destination that transports you to a bygone era of regal splendor. This city is a living testament to India's rich history and cultural heritage. Its stunning Amber Fort perched atop a hill, with its intricate architecture and breathtaking vistas, never ceases to amaze visitors. The City Palace, a sprawling complex of palaces, courtyards, and museums, provides a glimpse into the opulent lifestyle of the erstwhile Maharajas. Hawa Mahal, with its delicate honeycomb-like windows, is an architectural masterpiece that leaves you spellbound. The Jantar Mantar observatory, a UNESCO World Heritage Site, showcases the profound astronomical knowledge of the Rajput rulers.
+                        `}
                     </p>
                     <button className={styles.button}>Read More</button>
                 </div>
